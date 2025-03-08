05a88af53d4df7bbfd7b2242c58b0b3eef617bd3
Matias Rivas <matias.rivas.aa@gmail.com>
2025-03-08 15:53:32 -0300

    feat[part2]: solve up to 2.10 exercise

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <div>{person.name} {person.phone}</div>
+  )
+}
+
+const Filter = ({filterName, setFilterName}) => {
+  return (
+    <>
+      filter shown with
+      <input value={filterName} onChange={(e) => setFilterName(e.target.value)}/>
+    </>
+  )
+}
+
+const PersonForm = ({onSubmit, newName, setNewName, newNumber, setNewNumber}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+const Persons = ({ persons }) => {
+  return (
+    <>
+      {persons.map((person) => (<Person key={person.name} person={person}/>))}
+    </>
+  )
+}
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [filterName, setFilterName] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName,
+      phone: newNumber
+    }
+
+    const names = persons.map((person) => person.name)
+
+    if (names.indexOf(newName) === -1){
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+    else {
+      alert(`${newName} is already added to phoneboox`)
+    }
+  }
+
+  const filterNameLowerCase = filterName.toLowerCase()
+  const filteredPersons = persons.filter(({name}) => name.toLowerCase().includes(filterNameLowerCase))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <Filter filterName={filterName} setFilterName={setFilterName}/>
+      <h2>Add a new</h2>
+        <PersonForm onSubmit={addPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber = {setNewNumber} />
+      <h2>Numbers</h2>
+        <Persons persons={filteredPersons}/>
+    </div>
+  )
+}
+
+export default App
